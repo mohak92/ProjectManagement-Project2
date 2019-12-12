411e767f912bcddb8d92295a55ff058bc94ec791
Shehryar Ehtesham <se318@rutgers.edu>
2019-12-12 00:06:10 -0500

    made changes to models folder

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 
 // Starting the server, syncing our models ------------------------------------/
