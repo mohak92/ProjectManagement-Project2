9b3b05facdd3d42897a52200e933579d7ba19508
Shehryar Ehtesham <se318@rutgers.edu>
2019-12-11 21:26:08 -0500

    made new model associations

---
+++ config/task.js
@@ -0,0 +1,20 @@
+// module.exports = function (sequelize, DataTypes) {
+//     Task = sequelize.define("Task", {
+//         userId: {
+//             type: DataTypes.INTEGER
+//         },
+//         projectId: {
+//             type: DataTypes.INTEGER
+//         },
+//         taskName: {
+//             type: DataTypes.STRING
+//         },
+//         taskDueDate: {
+//             type: DataTypes.DATE
+//         }
+//     });
+//     // User.associate = models => {
+//     // User.hasMany(models.Projects);
+//     // });
+//     return Task;
+// };
--- models/project.js
+++ models/project.js
@@ -1,6 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-    var Project = sequelize.define("Project", {
-
+    this.Project = sequelize.define("Project", {
         projectId: {
             type: DataTypes.INTEGER
 
@@ -21,10 +20,61 @@ module.exports = function (sequelize, DataTypes) {
         projectStage: {
             type: DataTypes.STRING
         },
-    },
-        Project.associate = models => {
-            Project.hasMany(models.Task)
-            Projects.belongsTo(models.User)
-        });
+    }),
+    this.Task = sequelize.define("Task", {
+        userId: {
+            type: DataTypes.INTEGER
+        },
+        projectId: {
+            type: DataTypes.INTEGER
+        },
+        taskName: {
+            type: DataTypes.STRING
+        },
+        taskDueDate: {
+            type: DataTypes.DATE
+        }
+    }),
+    this.User = sequelize.define("User", {
+        task_id: {
+              type: DataTypes.STRING,
+              allowNull: false,
+              validate: {
+                  len: [1,8]
+              }
+          },
+          project_id: {
+              type: DataTypes.TEXT,
+              allowNull: false,
+              validate: {
+                  len: [1,10]
+              }
+          },
+          email: {
+              type: DataTypes.STRING,
+              defaultValue: "Personal"
+          },
+          password: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        dateOfBirth: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN
+        }
+    });
+    this.Project.hasMany(this.Task);
+    this.Project.belongsTo(this.User)
     return Project;
 };
