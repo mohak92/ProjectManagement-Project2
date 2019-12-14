7c8d1947fc9de906aa7195a2c44e54d05614742c
Shehryar Ehtesham <se318@rutgers.edu>
2019-12-14 05:41:11 -0500

    added to passport authentication

--- server.js
+++ server.js
@@ -2,25 +2,30 @@ require("dotenv").config();
 var express = require("express");
 var db = require("./models");
 var app = express();
-// var passport = require('passport')
-// var session = require('express-session')
-var bodyParser = require('body-parser')
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
 // Middleware
+    //For Express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+  //For Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+    //For Passport
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
 app.use(passport.initialize());
-
 app.use(passport.session());
 
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+var authRoute = require('./routes/auth.js')(app, passport);
+require('./config/passport/passport.js')(passport, db.user);
+
 
 var syncOptions = { force: true };
 
