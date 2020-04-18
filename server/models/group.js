7875c948229781fc3ea76ffefa9a5c7b2a5ece75
Shehryar Ehtesham <se318@rutgers.edu>
2020-04-17 21:08:21 -0400

    finished group model

---
+++ server/models/group.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const GroupSchema = new Schema({
+  name: {
+    type: String,
+    default: 'Awesone Group'
+  },
+  imageUrl: {
+    type: String
+  },
+  users: {
+    type: Array,
+  },
+  tasks: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Task'
+  }],
+  timeStamp: {
+      type: Date
+  }
+});
+
+
+module.exports = Group =  mongoose.model('groups', GroupSchema);
