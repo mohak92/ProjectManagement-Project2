6d6ebd710e02284bd1b297db6ef4ae8ff9a6f673
Shehryar Ehtesham <se318@rutgers.edu>
2020-04-19 22:58:09 -0400

    group controller started

---
+++ server/controllers/group.js
@@ -0,0 +1,16 @@
+const Group = require('../models/group');
+
+//import validateGroupInput from group.js
+
+exports.getGroups = [
+    function (req, res) {
+        Group.find({}, (err, groups) => {
+            if (err) return res.status(404).send({ error: err });
+            return res.json({
+                success: true,
+                groups
+            });
+        });
+    }
+];
+
